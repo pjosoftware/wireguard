ea460a0c5293131b90ded71a5bd6577a129d6339
PJO Software <you@example.com>
2025-08-09 14:08:50 -0700

    Adding port to wg container on server; Updating firewall script to take in port defined in env file

--- server/wg_specific_firewall_network_setup.sh
+++ server/wg_specific_firewall_network_setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+WG_PORT=$(grep '^WG_PORT=' ./env | cut -d '=' -f2)
+
+if [[ -z "$WG_PORT" ]]; then
+  echo "Error: WG_PORT not set in env file."
+  exit 1
+fi
+
 sudo echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo ufw allow 51820/udp
+sudo ufw allow "${WG_PORT}/udp"
