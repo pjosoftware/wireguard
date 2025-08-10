2d111d453133dcdcc6ea3264cfdd4a98028be965
PJO Software <you@example.com>
2025-08-09 21:13:43 -0700

    changing postup and preup for server wg conf

---
+++ server/wireguard/apply_rules.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+RULES_FILE="$SCRIPT_DIR/port_forwarding_rules.conf"
+ACTION="$1"  # "add" or "delete"
+
+if [[ "$ACTION" == "add" ]]; then
+    iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE
+elif [[ "$ACTION" == "delete" ]]; then
+    iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth+ -j MASQUERADE
+fi
+
+while IFS=: read -r DEST_IP PORT PROTO; do
+  if [[ "$ACTION" == "add" ]]; then
+    iptables -t nat -A PREROUTING -p "$PROTO" --dport "$PORT" -j DNAT --to-destination "$DEST_IP:$PORT"
+  elif [[ "$ACTION" == "delete" ]]; then
+    iptables -t nat -D PREROUTING -p "$PROTO" --dport "$PORT" -j DNAT --to-destination "$DEST_IP:$PORT"
+  fi
+done < "$RULES_FILE"
